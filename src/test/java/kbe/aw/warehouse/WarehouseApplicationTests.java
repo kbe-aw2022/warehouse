fd393c9146b04a4b755b28e21c04324a66fe6cd4
Artur <artur.horch@gmx.de>
2022-04-20 19:56:35 +0200

    initial commit

---
+++ src/test/java/kbe/aw/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,15 @@
+package kbe.aw.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests
+{
+
+   @Test
+   void contextLoads()
+   {
+   }
+
+}
